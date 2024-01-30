6410addfa0dcde013b56d2a9a2024d419ad2c221
Afzal Ansari <afzal442@gmail.com>
2024-01-30 08:57:51 +0000

    fmts the health check

--- scripts/app_health_check.sh
+++ scripts/app_health_check.sh
@@ -7,11 +7,9 @@ check_application_status() {
     http_status=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL")
 
     if [ "$http_status" -eq 200 ]; then
-        echo "Application is UP. HTTP Status Code: $http_status"
-        echo "It is available"
+        echo "Application is available with HTTP status code $http_status"
     else
-        echo "Application is DOWN. HTTP Status Code: $http_status"
-        echo "It is unavailable or not responding"
+        echo "Application is unavailable or not responding with HTTP status code $http_status"
     fi
 }
 
